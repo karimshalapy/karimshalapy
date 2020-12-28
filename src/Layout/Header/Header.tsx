c70499ea8f37b0b76c19182920fbbc2cd8164326
Karim Shalapy <karimshalapy@gmail.com>
2020-12-28 18:46:45 +0200

    feat: hide and show navbar depending on scroll state

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../../components/Button'
+import useScrollDirection from '../../hooks/useScrollDirection'
 import classes from './Header.module.css'
 import NavItems from './NavItems/NavItems'
 
@@ -8,8 +9,27 @@ interface Props {
 }
 
 const Header: React.FC<Props> = props => {
+    const scrollDirection = useScrollDirection({ initialDirection: "down" })
+    const [scrolledToTop, setScrolledToTop] = useState(true)
+
+    const handleScroll = () => {
+        setScrolledToTop(window.pageYOffset < 50)
+    }
+    //useEffect to check if we're at top on scroll
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+    //creating a variable with the classes of the header depending on the scroll state
+    const headerClasses = [classes.Header,
+    scrollDirection === "down" && !scrolledToTop ? classes.ScrolledDown : "",
+    scrollDirection === "up" && !scrolledToTop ? classes.ScrolledUp : "",
+    ].join(" ")
+
     return (
-        <header className={classes.Header}>
+        <header className={headerClasses}>
             <nav className={classes.Navbar}>
                 <p>K</p>
                 <div className={classes.NavItemsContainer}>
