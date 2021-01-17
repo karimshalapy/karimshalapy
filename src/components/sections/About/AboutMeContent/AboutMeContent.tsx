a5a6681484c0225a1e8adb74845448107e3062b9
Karim Shalapy <karimshalapy@gmail.com>
2021-01-17 21:48:14 +0200

    chore: change job description in About and Hero

--- src/components/sections/About/AboutMeContent/AboutMeContent.tsx
+++ src/components/sections/About/AboutMeContent/AboutMeContent.tsx
@@ -9,7 +9,7 @@ interface Props {
 const AboutMeContent: React.FC<Props> = props => {
     return (
         <div>
-            <p>Hello! I'm Karim, a software engineer based in Egypt.</p>
+            <p>Hello! I'm Karim, a front-end developer based in Egypt.</p>
             <p>I love creating things using JavaScript in general, and in React specifically. My goal is to always build products that provide pixel-perfect, performant experiences.</p>
             <p>I graduated from <InlineLink href="http://suezuniv.edu.eg/su/index.php/en/">Suez University</InlineLink> and worked as a Welding QC Engineer for 3 years. After that, I figured out that's not what I wanted to do in life, so I shifted my career to become a software engineer.</p>
             <p>Here are a few technologies I've been working with recently:</p>
