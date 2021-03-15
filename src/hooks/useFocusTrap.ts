52c7db6948ca44e4ecf1cde6f8d3fa595d108f56
Karim Shalapy <karimshalapy@gmail.com>
2021-03-15 21:27:23 +0200

    refactor: seperate keyCodes into utils folder

--- src/hooks/useFocusTrap.ts
+++ src/hooks/useFocusTrap.ts
@@ -1,17 +1,5 @@
 import { useEffect } from "react"
-
-const KEY_CODES = {
-    ARROW_LEFT: 'ArrowLeft',
-    ARROW_LEFT_IE11: 'Left',
-    ARROW_RIGHT: 'ArrowRight',
-    ARROW_RIGHT_IE11: 'Right',
-    ESCAPE: 'Escape',
-    ESCAPE_IE11: 'Esc',
-    TAB: 'Tab',
-    SPACE: ' ',
-    SPACE_IE11: 'Spacebar',
-    ENTER: 'Enter',
-}
+import KEY_CODES from '../utils/keyCodes'
 
 export const useFocusTrap = (
     buttonRef: React.RefObject<HTMLButtonElement>,
