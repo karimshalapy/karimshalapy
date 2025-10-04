787e616fcbc06e3d995d4a57fc08c1f2eb9b777f
Karim Shalapy <karimshalapy@gmail.com>
2025-10-04 21:26:39 +0300

    refactor: remove all logic

--- src/MainContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-import { IAllAppData } from './@types/Work'
-
-export const MainContext = createContext<IAllAppData>({
-    projects: [],
-    otherProjects: [],
-    jobsDetails: [],
-})
-export default MainContext
